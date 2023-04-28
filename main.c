b80a4e6574926ef3104e794aadf6b11b6fbd8c2b
azizios011 <azizhidri001@gmail.com>
2023-04-27 21:58:11 -1100

    the core of shell

---
+++ main.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * main - The Gates of shell
+ * @argc:
+ * @argv: 
+ * Return: EXIT_SUCCESS
+*/
+int main(int argc, char **argv)
+{
+    loop();
+    return (EXIT_SUCCESS);
+}
