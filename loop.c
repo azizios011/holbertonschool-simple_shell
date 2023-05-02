f21074a050beb3dc7827be76b5db3a69485952c2
azizios011 <azizhidri001@gmail.com>
2023-05-02 03:19:20 -1100

    fix

--- loop.c
+++ loop.c
@@ -1,47 +1,24 @@
 #include"main.h"
 /**
- * loop - the main function
+ * loop - check the code
  *
- * Return: Always 0.
+ * Return: EXIT_SUCCESS.
 */
-void loop(void)
+int loop(void)
 {
 	char *line;
-	char *args[MAX_ARGS];
-	int num_args;
-	char *cmd_path;
+	char **args;
+	int status;
 
-	char *paths[] = {"/bin", "/usr/bin", NULL};
-
-	while (1)
-	{
+	do {
 		printf("($) ");
-		line = read_cmd_line();
-		num_args = parse_cmd_line(line, args);
-		if (num_args > 0)
-		{
-			if (strcmp(args[0], "exit") == 0)
-			{
-			exit_builtin(args);
-			}
-			else if (strcmp(args[0], "env") == 0)
-			{
-			env_builtin(args);
-			}
-			else
-			{
-				cmd_path = get_cmd_path(args[0], paths);
-				if (cmd_path != NULL)
-				{
-				execute_cmd(args);
-				free(cmd_path);
-				}
-				else
-				{
-					printf("Command not found\n");
-			}
-			}
-		}
+		line = read_line();
+		args = split_line(line);
+		status = execute(args);
+
 		free(line);
-	}
+		free(args);
+	} while (status);
+
+	return (EXIT_SUCCESS);
 }
